b69abe438c930bacc5aded6065cba0df9455e2c8
Emil Thorup <etho@jysk.com>
2026-06-03 08:57:00 +0200

    ny

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+def besked(navn):
+    """
+    Returnerer en Hello World besked.
+
+    Args:
+        navn (str): Navnet på personen.
+
+    Returns:
+        str: En hilsen.
+    """
+    return f"Hello World, {navn}"
+
+
+navne = ["Anders", "Mette", "Peter"]
+
+for navn in navne:
+    if navn == "Anders":
+        print(besked(navn))
+    elif navn == "Mette":
+        print(besked(navn))
+    else:
+        print(f"Hello World, ukendt bruger: {navn}")
